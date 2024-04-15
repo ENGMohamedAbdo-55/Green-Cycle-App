decb5799e370eed51e2848bb17977a0a2aa41730
MarkFathy <markfathy192@gmail.com>
2024-04-15 16:58:05 +0200

    commit auth

--- lib/Features/auth/login/screen/login_screen.dart
+++ lib/Features/auth/login/screen/login_screen.dart
@@ -18,165 +18,151 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var cubit = LoginCubit.get(context);
     return Scaffold(
-      appBar:const CustomAppBar(
+      appBar: const CustomAppBar(
         title: 'مرحبا بك',
       ),
-      body: BlocConsumer<LoginCubit, LoginStates>(
-        listener: (context, state){
-          if (state is LoginSuccessState) {
-            ScaffoldMessenger.of(context).showSnackBar(
-              const SnackBar(
-                content: Text('تم الدخول بنجاح'),
-                duration: Duration(seconds: 2),
-                backgroundColor: Colors.green, // Adjust duration as needed
+      body: Form(
+        key: cubit.formKey,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const Image(
+                image: AssetImage('assets/images/logo/logo.png'),
               ),
-              // navigateAndFinish(context, HomeScreen());
-            );
-          } else if (state is LoginErrorState) {
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(
-                content: Text(state.error),
-                duration:const Duration(seconds: 2),
-                backgroundColor: Colors.red, // Adjust duration as needed
-              ),
-            );
-          }
-
-          if (state is PasswordResetEmailSentState) {
-            // Show a dialog or Snackbar to inform the user that a password reset email has been sent
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(
-                content: Text('Password reset email sent. Check your email inbox.'),
-                duration: Duration(seconds: 3),
-                backgroundColor: Colors.green,
-              ),
-            );
-          } else if (state is PasswordResetErrorState) {
-            // Show a dialog or Snackbar to inform the user about the error in password reset process
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(
-                content: Text(state.error),
-                duration: Duration(seconds: 3),
-                backgroundColor: Colors.red,
-              ),
-            );
-          }
-
-
-
-        },
-        builder: (context, state) {
-          var cubit = LoginCubit.get(context);
-          return Form(
-            key: cubit.formKey,
-            child: Column(
-              children: [
-                const Image(
-                  image: AssetImage('assets/images/logo/logo.png'),
-                ),
-                SizedBox(height:25.h),
-                Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: DefaultTextField(
-                    keyboardType: TextInputType.emailAddress,
-                    controller: cubit.emailController,
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return "البريد الالكتروني لا يجب ان يكون فارغ";
-                      }
-                      return null;
-                    },
-                    hintText: 'البريد الالكتروني',
-                    suffixIcon: const Icon(Icons.email),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: DefaultTextField(
-                    keyboardType: TextInputType.text,
-                    obsecure: cubit.isSecured,
-                    controller: cubit.passController,
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return "كلمه المرور لا يجب ان تكون فارغه";
-                      }
-                      return null;
-                    },
-                    hintText: 'كلمة المرور',
-                    suffixIcon: const Icon(Icons.lock),
-                    prefixIcon: cubit.togglePass(),
-                     onFieldSubmitted: (value) {
-                       if (cubit.formKey.currentState!.validate()) {
-                         cubit.userLogin(
-                           email: cubit.emailController.text,
-                           password: cubit.passController.text,
-                           context: context,
-                         );
-                       }
-                     }
-                     ),
-                ),
-                Row(
-                  children: [
-                    TextButton(
-                      onPressed: () {
-                        navigateTo(context,const ResetPassScreen());
-                      },
-                      child: Text('نسيت كلمه المرور',
-                          style: TextStyle(
-                              fontSize: 12.sp,
-                              color: MyColors.greenColor,
-                              decoration: TextDecoration.underline,
-                              decorationColor: MyColors.greenColor)),
-                    )
-                  ],
-                ),
-                if (state is LoginLoadingState)
-                  CircularProgressIndicator(color: MyColors.greenColor)
-                else
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 20,top: 20,left: 10,right: 10),
-                  child: FirstBtn(text1: 'تسجيل الدخول', onPressed: ()async{
-
-                    if (cubit.formKey.currentState!.validate()) {
-                      cubit.userLogin(
-                        context: context,
-                        email: cubit.emailController.text,
-                        password: cubit.passController.text,
-                      );
-
+              SizedBox(height: 25.h),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: DefaultTextField(
+                  keyboardType: TextInputType.emailAddress,
+                  controller: cubit.emailController,
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "البريد الالكتروني لا يجب ان يكون فارغ";
                     }
-
-
-                  }),
+                    return null;
+                  },
+                  hintText: 'البريد الالكتروني',
+                  suffixIcon: const Icon(Icons.email),
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    TextButton(onPressed: (){
-                      navigateTo(context,const RegisterScreen());
-                    }, child:Text('انشاء حساب جديد',
-                      style:TextStyle(
-                      color: MyColors.greenColor,
+              ),
+              BlocBuilder<LoginCubit, LoginStates>(
+                builder: (context, state) {
+                  return Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: DefaultTextField(
+                        keyboardType: TextInputType.text,
+                        obsecure: cubit.isSecured,
+                        controller: cubit.passController,
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'كلمه المرور لا يجب ان تكون فارغه';
+                          } else if (value.length < 6) {
+                            return 'كلمه المرور لا يجب ان تكون اقل من 6';
+                          }
+                          return null;
+                        },
+                        hintText: 'كلمة المرور',
+                        suffixIcon: const Icon(Icons.lock),
+                        prefixIcon: cubit.togglePass(),
+                        onFieldSubmitted: (value) {
+                          if (cubit.formKey.currentState!.validate()) {
+                            cubit.userLogin(
+                              email: cubit.emailController.text,
+                              password: cubit.passController.text,
+                              context: context,
+                            );
+                          }
+                        }),
+                  );
+                },
+              ),
+              Row(
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      navigateTo(context, const ResetPassScreen());
+                    },
+                    child: Text('نسيت كلمه المرور',
+                        style: TextStyle(
+                            fontSize: 12.sp,
+                            color: MyColors.greenColor,
+                            decoration: TextDecoration.underline,
+                            decorationColor: MyColors.greenColor)),
+                  )
+                ],
+              ),
+              BlocConsumer<LoginCubit, LoginStates>(
+                listener: (context, state) {
+                  if (state is LoginSuccessState) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                        content: Text('تم الدخول بنجاح'),
+                        duration: Duration(seconds: 2),
+                        backgroundColor:
+                        Colors.green, // Adjust duration as needed
                       ),
-
+                      // navigateAndFinish(context, HomeScreen());
+                    );
+                  } else if (state is LoginErrorState) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text(state.error),
+                        duration: const Duration(seconds: 2),
+                        backgroundColor:
+                        Colors.red, // Adjust duration as needed
+                      ),
+                    );
+                  }
+                  if (state is LoginLoadingState) {
+                    CircularProgressIndicator(color: MyColors.greenColor);
+                  }
+                },
+                builder: (context, state) {
+                  return Padding(
+                    padding: const EdgeInsets.only(
+                        bottom: 20, top: 20, left: 10, right: 10),
+                    child: FirstBtn(
+                        text1: 'تسجيل الدخول',
+                        onPressed: () async {
+                          if (cubit.formKey.currentState!.validate()) {
+                            cubit.userLogin(
+                              context: context,
+                              email: cubit.emailController.text,
+                              password: cubit.passController.text,
+                            );
+                          }
+                        }),
+                  );
+                },
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      navigateTo(context, const RegisterScreen());
+                    },
+                    child: Text(
+                      'انشاء حساب جديد',
+                      style: TextStyle(
+                        color: MyColors.greenColor,
+                      ),
                     ),
-                    ),
-                    Text('ليس لديك حساب؟',
+                  ),
+                  Text(
+                    'ليس لديك حساب؟',
                     style: AppStyles.textStyle11,
-                    ),
-                  ],
-                ),
-                const MyDivider(),
-                SizedBox(height: 20.h),
-                const GoogleAndFace(),
-
-
-              ],
-            ),
-          );
-        },
+                  ),
+                ],
+              ),
+              const MyDivider(),
+              SizedBox(height: 20.h),
+              const GoogleAndFace(),
+            ],
+          ),
+        ),
       ),
     );
   }
---
+++ lib/Features/home/view/layouts/user_account.dart
@@ -0,0 +1,46 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:green_cycle_app/Features/auth/register/contents.dart';
+
+import '../../../../core/components/colors.dart';
+
+class UserAccount extends StatelessWidget {
+  const UserAccount({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: CustomAppBar(
+        leading:IconButton(onPressed: (){
+          Navigator.pop(context);
+        },
+          icon:const Icon(Icons.arrow_back_ios),color: MyColors.whiteColor,),
+        title: 'تعديل الملف',
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Stack(
+              alignment: Alignment.bottomRight,
+              children: [
+                CircleAvatar(
+                  radius: 70.r,
+                  backgroundColor: MyColors.greyColor,
+                ),
+                IconButton(
+                  onPressed: () {
+                    // Add your onPressed functionality here
+                  },
+                  icon: Icon(Icons.add_a_photo),
+                  color: Colors.black,
+                  iconSize: 30.0.r,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
